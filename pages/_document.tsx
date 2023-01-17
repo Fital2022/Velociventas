792ee5704312c40eb398f9ec904086e0c87a3988
Brian <starly_balli@hotmail.com>
2023-01-17 10:27:38 -0600

    analitics configure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,49 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-LJ0XR6PM0Y`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LJ0XR6PM0Y', {
+                page_path: window.location.pathname,
+              });
+          `
+            }}
+          />
+          <Script id="facebook-pixel">
+      {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', 1189353171955805);
+        fbq('track', 'PageView');
+      `}
+    </Script>
+          <noscript><img height="1" width="1"
+          src="https://www.facebook.com/tr?id=1189353171955805&ev=PageView&noscript=1"
+          /></noscript>
+        </Head>
       <body>
         <Main />
         <NextScript />
