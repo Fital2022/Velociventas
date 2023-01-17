cdd7661b482aeb7c49c42a0cbcaaa107d16afb96
Gustavo Leonardo <leonardomoralesgustavo@gmail.com>
2023-01-17 11:57:57 -0600

    update whatsapp button style

--- components/buttons/WhatsAppButton.tsx
+++ components/buttons/WhatsAppButton.tsx
@@ -21,7 +21,6 @@ const WhatsAppButton = () => {
           left: "0px",
           border: "1px solid",
           borderColor: "#25D366",
-          backgroundColor: "#25D366",
           bgcolor: "rgb(47, 111, 165)", 
           color: "white" 
         }}
