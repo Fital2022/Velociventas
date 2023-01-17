1a480494345f8463a10deb4bcde232c3a5e2e90c
Gustavo Leonardo <leonardomoralesgustavo@gmail.com>
2023-01-17 12:14:40 -0600

    update effect whatsapp button

--- components/buttons/WhatsAppButton.tsx
+++ components/buttons/WhatsAppButton.tsx
@@ -22,7 +22,7 @@ const WhatsAppButton = () => {
           border: "1px solid",
           borderColor: "#25D366",
           backgroundColor: "#25D366",
-          bgcolor: "#25D366", color: "white" 
+          bgcolor: "#25D366" 
         }}
         aria-label="save"
         onClick={WhatsApp}
